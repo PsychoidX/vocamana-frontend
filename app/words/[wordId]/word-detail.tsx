f6685bb33123919311761edcb333bab97216f0b4
PsychoidX <psychoidx99@gmail.com>
2023-11-04 20:58:32 +0900

    単語詳細ページに関連文章を表示する機能を追加

--- app/words/[wordId]/word-detail.tsx
+++ app/words/[wordId]/word-detail.tsx
@@ -8,6 +8,12 @@ type Word = {
   user_id: number;
 }
 
+type Sentence = {
+  id: number;
+  sentence: string;
+  user_id: number;
+}
+
 async function getWordById(wordId: string): Promise<Word> {
   const numWordId = Number(wordId);
   
@@ -26,16 +32,63 @@ async function getWordById(wordId: string): Promise<Word> {
         throw new Error(`Error: No Response. ${err.message}`);
       }
     });
-    return words;
+
+  return words;
+}
+
+async function getSentencesByWordId(wordId: string): Promise<Sentence[]> {
+  const numWordId = Number(wordId);
+  
+  if(isNaN(numWordId)) {
+    throw new Error(`Error: ${wordId} is invalid word id.`);
+  }
+
+  const sentences = axios
+    .get(`http://localhost:8081/words/${wordId}/associated-sentences`)
+    .then((res) => res.data)
+    .catch((err) => {
+      if(err.response) {
+        console.log(`Error: ${err.message} (${err.response.message})`);
+        return err.response;
+      } else {
+        throw new Error(`Error: No Response. ${err.message}`);
+      }
+    });
+
+  return sentences;
+}
+
+function AssociatedSentences(props: {wordId: string}) {
+  const { wordId } = props;
+  const sentences: Sentence[] = use(getSentencesByWordId(wordId))
+  if(sentences==null) {
+    return <div>登録された文章はありません</div>
+  } else {
+    return (
+      <div>
+        {sentences.map((sentence) => (
+          <div
+            style={{
+              marginBottom: '10px'
+            }}
+          >
+            {sentence.sentence}
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default function WordDetail(props: {wordId: string}) {
   const { wordId } = props;
   const word: Word = use(getWordById(wordId));
+
   return (
     <>
       <h2>{word.word}</h2>
       <p>{word.memo}</p>
+      <AssociatedSentences wordId={wordId} />
     </>
   );
 }
