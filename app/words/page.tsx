6eb1704094906cd05115d7ae938a75288e48dab6
PsychoidX <psychoidx99@gmail.com>
2023-11-05 22:45:30 +0900

    AllWordListコンポーネントを別ファイルに切り分け

--- app/words/page.tsx
+++ app/words/page.tsx
@@ -1,44 +1,5 @@
-import { use } from "react"
-import axios from "axios"
 import Link from "next/link"
-
-async function getAllWords() {
-  const words = axios
-    .get("http://localhost:8081/words")
-    .then((res) => res.data)
-    .catch((err) => {
-      if(err.response) {
-        console.log(`Error: ${err.message} (${err.response.message})`);
-        return err.response;
-      } else {
-        throw new Error(`Error: No Response. ${err.message}`);
-      }
-    });
-    return words;
-}
-
-function AllWordsList() {
-  const words: Word[] = use(getAllWords());
-  if(words === null) {
-    return <p>登録済みの単語はありません</p>;
-  } else {
-    return(
-      <div>
-        {words.map((word) => (
-          <Link
-            key={word.id}
-            href={`words/${word.id}`}
-            style={{
-              marginRight: '10px'
-            }}
-          >
-            {word.word}
-          </Link>
-        ))}
-      </div>
-    );
-  }
-}
+import AllWordsList from "./all-word-list"
 
 export default function Home() {
   return (
