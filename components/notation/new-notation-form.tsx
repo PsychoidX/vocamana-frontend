7d61fde90c4044e8a94eb7590365eb126f5c1f0e
PsycohidX <psychoidx99@gmail.com>
2023-11-10 23:36:50 +0900

    NewNotationFormコンポーネントを独立したファイルに切り分け

---
+++ components/notation/new-notation-form.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { addNotation } from "@/api/notations";
+import { useForm } from "react-hook-form";
+
+type NotationFormValues = {
+  notation: string
+}
+
+export default function NewNotationForm(
+  props: {
+    wordId: string,
+    setIsActice: (isActive: boolean)=>void,
+  }) {
+  const { wordId, setIsActice } = props;
+  const { register, handleSubmit } = useForm<NotationFormValues>();
+
+  const onSubmit = async (data: NotationFormValues) => {
+    const notation = data.notation;
+    const success = await addNotation(wordId, notation);
+
+    if(success) {
+      setIsActice(false);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} >
+      <label htmlFor="notation">新規表記揺れ:</label>
+      <input {...register("notation", {required: true})} />
+      <input type="submit" />
+    </form>
+  );
+}
--- components/notation/notation-modal.tsx
+++ components/notation/notation-modal.tsx
@@ -1,40 +1,9 @@
 "use client"
 
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
 import { Button } from "@/components/common/button";
 import Modal from "@/components/common/modal";
-import { addNotation } from "@/api/notations";
-
-type NotationFormValues = {
-  notation: string
-}
-
-function NewNotationForm(
-  props: {
-    wordId: string,
-    setIsActice: (isActive: boolean)=>void,
-  }) {
-  const { wordId, setIsActice } = props;
-  const { register, handleSubmit } = useForm<NotationFormValues>();
-
-  const onSubmit = async (data: NotationFormValues) => {
-    const notation = data.notation;
-    const success = await addNotation(wordId, notation);
-
-    if(success) {
-      setIsActice(false);
-    }
-  }
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} >
-      <label htmlFor="notation">新規表記揺れ:</label>
-      <input {...register("notation", {required: true})} />
-      <input type="submit" />
-    </form>
-  );
-}
+import NewNotationForm from "@/components/notation/new-notation-form"
 
 // クリックでNotationModalを表示させるボタン
 export function NotationModalToggleButton(
