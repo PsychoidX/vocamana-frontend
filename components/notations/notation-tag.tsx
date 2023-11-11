776146c1398bbce538f155281bcfc04814caed30
PsycohidX <psychoidx99@gmail.com>
2023-11-12 04:42:09 +0900

    機能修正
    NotationTagを、削除された後はタグを非表示にするよう修正

--- components/notations/notation-tag.tsx
+++ components/notations/notation-tag.tsx
@@ -1,22 +1,29 @@
 'use client'
 import { deleteNotation } from "@/api/notations";
 import { Tag } from "@/components/common/tag";
+import { useState } from "react";
 
 export default function NotationTagWithDeleteButton(props: { notation: Notation }) {
   const { notation } = props;
+  const [isVisible, setIsVisible] = useState(true);
+
   async function handleDelete(notationId: string) {
     const success = await deleteNotation(notationId);
     if(success) {
-      // TODO: 一覧から対応Notationを非表示
+      setIsVisible(false);
     }
   }
 
-  return(
-    <Tag
-      showDeleteButton={true}
-      onDeleteButtonClick={ () => { handleDelete(String(notation.id)) }}
-    >
-      { notation.notation }
-    </Tag>
-  )
+  if(isVisible) {
+    return(
+      <Tag
+        showDeleteButton={true}
+        onDeleteButtonClick={ () => { handleDelete(String(notation.id)) }}
+      >
+        { notation.notation }
+      </Tag>
+    );
+  } else {
+    return <></>
+  }
 }
