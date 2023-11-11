600550ad50ae6d7a4def2f3b0d394b42e1503f74
PsycohidX <psychoidx99@gmail.com>
2023-11-11 23:48:01 +0900

    リファクタリング
    word-detail.tsxに書いていた処理を、APIを別ファイルに分離しつつ、page.tsxに移動

--- api/words.ts
+++ api/words.ts
@@ -1,5 +1,25 @@
 'use server'
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+
+export async function getWordById(wordId: string): Promise<Word|null> {
+  if(isNaN(Number(wordId))) {
+    console.log(`Error: ${wordId} is invalid word id.`);
+    return null;
+  }
+
+  try {
+    const res: AxiosResponse<Word> = await axios.get(`http://localhost:8081/words/${wordId}`);
+    const word: Word = res.data;
+    return word;
+  } catch(err: any) {
+    if(err.response) {
+      console.log(`Error: ${err.message} (${err.response.message})`);
+    } else {
+      console.log(`Error: No Response. ${err.message}`);
+    }
+    return null;
+  }  
+}
 
 export async function addWord(word: string, memo: string): Promise<boolean> {
   try {
--- app/words/[wordId]/page.tsx
+++ app/words/[wordId]/page.tsx
@@ -1,22 +1,33 @@
-import WordDetail from "./word-detail";
 import WordDeleteButton from "@/components/word/word-delete-button";
 import { NotationModalToggleButton } from "@/components/notation/notation-modal";
 import AllNotationsList from "@/components/notation/all-notations-list";
+import { use } from "react"
+import AssociatedSentencesList from "@/components/word/associated-sentences-list";
+import { getWordById } from "@/api/words";
 
 export default function WordDetailPage({ params }: {params: {wordId: string}}) {
-  return (
-    <>
-      <WordDetail wordId={params.wordId} />
-      <WordDeleteButton
-        wordId={params.wordId}
-        redirectTo="/words"
-      />
-      <NotationModalToggleButton
-        wordId={params.wordId}
-        allWordNotationListComponent={<AllNotationsList wordId={params.wordId} />}  
-      >
-          表記揺れ追加
-      </NotationModalToggleButton>
-    </>
-  );
+  const { wordId } = params;
+  const word = use(getWordById(wordId));
+
+  if(word) {
+    return(
+      <>
+        <h2>{ word.word }</h2>
+        <p>{ word.memo }</p>
+        <AssociatedSentencesList wordId={wordId} />
+        <WordDeleteButton
+          wordId={wordId}
+          redirectTo="/words"
+        />
+        <NotationModalToggleButton
+          wordId={wordId}
+          allWordNotationListComponent={<AllNotationsList wordId={wordId} />}  
+        >
+            表記揺れ追加
+        </NotationModalToggleButton>
+      </>
+    )
+  } else {
+    return <p>該当する単語は見つかりませんでした</p>
+  }
 }
