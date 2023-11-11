d986c08242ab41437247efe560aeb919d7ed385d
PsycohidX <psychoidx99@gmail.com>
2023-11-12 01:28:15 +0900

    Tagコンポーネントを追加

---
+++ components/common/tag.tsx
@@ -0,0 +1,30 @@
+import classNames from "classnames";
+import React, { useState } from "react";
+
+type TagProps = {
+  showDeleteButton?: boolean,
+  onDeleteButtonClick?: ()=>void,
+  children: React.ReactNode,
+  additionalClassNames?: string,
+}
+
+export function Tag(props: TagProps) {
+  const { showDeleteButton, onDeleteButtonClick, children, additionalClassNames} = props;
+  
+  let deleteButton: React.ReactNode|undefined;
+  if(showDeleteButton) {
+    deleteButton = <button className="delete" onClick={onDeleteButtonClick}></button>
+  }
+
+  return (
+    <span 
+      className={classNames(
+        "tag",
+        "is-medium",
+        additionalClassNames || "",
+    )}>
+      { children }
+      { deleteButton }
+    </span>
+  )
+}
