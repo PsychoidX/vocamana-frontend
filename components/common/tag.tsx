770eb66ccdf7b6d5dd1de7d545791008adc7a95d
PsycohidX <psychoidx99@gmail.com>
2023-11-12 19:23:53 +0900

    Tagにhrefを指定できるよう修正・LargeTagを追加

--- components/common/tag.tsx
+++ components/common/tag.tsx
@@ -1,18 +1,28 @@
 import classNames from "classnames";
+import Link from "next/link";
+import React from "react";
 
 type TagProps = {
   showDeleteButton?: boolean,
   onDeleteButtonClick?: ()=>void,
   children: React.ReactNode,
   additionalClassNames?: string,
+  href?: string,
 }
 
 export function Tag(props: TagProps) {
-  const { showDeleteButton, onDeleteButtonClick, children, additionalClassNames} = props;
+  const { showDeleteButton, onDeleteButtonClick, children, additionalClassNames, href } = props;
   
   let deleteButton: React.ReactNode|undefined;
   if(showDeleteButton) {
     deleteButton = <button className="delete" onClick={onDeleteButtonClick}></button>
+  }
+
+  let childrenWithLink: React.ReactNode;
+  if(href) {
+    childrenWithLink = <Link href={ href }>{ children }</Link>;
+  } else {
+    childrenWithLink = children;
   }
 
   return (
@@ -22,8 +32,22 @@ export function Tag(props: TagProps) {
         "is-medium",
         additionalClassNames || "",
     )}>
-      { children }
+      { childrenWithLink }
       { deleteButton }
     </span>
   )
+}
+
+export function LargeTag(props: TagProps) {
+  const { children, href } = props;
+  return (
+    <Tag 
+      additionalClassNames="is-large"
+      href={ href }
+      showDeleteButton={ props.showDeleteButton }
+      onDeleteButtonClick={ props.onDeleteButtonClick }
+    >
+      { children }
+    </Tag>
+  );
 }
