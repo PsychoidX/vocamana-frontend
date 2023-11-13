1e58467615c8560c2761c03843f88a6a8c177e0a
PsycohidX <psychoidx99@gmail.com>
2023-11-14 01:31:47 +0900

    ConfirmDeleteModal, COnfirmDeleteModalOpenButtonを追加
    削除ボタン押下時に確認メッセージを表示するため

--- components/common/modal.tsx
+++ components/common/modal.tsx
@@ -48,3 +48,70 @@ export function Modal(
       </div>
   );
 }
+
+// 削除して良いかを確認するメッセージと
+// 「削除」「キャンセル」ボタンを含むモーダル
+function ConfirmDeleteModal(
+  props: {
+    isActive: boolean, // tureの間だけ表示
+    message: string,
+    onClickDelete: ()=>void,
+    onClickCancel: ()=>void,
+}) {
+  const { isActive, message, onClickDelete, onClickCancel } = props;
+
+  return(
+    <Modal
+      isActive={ isActive }
+    >
+      <p>{ message }</p>
+      <ButtonsArea>
+        <DangerButton onClick={ onClickDelete }>削除</DangerButton>
+        <Button onClick={ onClickCancel }>キャンセル</Button>
+      </ButtonsArea>
+    </Modal>
+
+  );
+}
+
+// クリックでConfirmDeleteButtonを表示させるボタン
+export function ConfirmDeleteModalOpenButton(
+  props: {
+    confirmMessage: string, // 例: 「xxxを本当に削除しますか？」
+    onClickDelete: ()=>void, // 表示されるモーダルの「削除」押下時の処理
+    onClickCancel?: ()=>void, // 表示されるモーダルの「キャンセル」押下時の処理
+    children: React.ReactNode,
+}) {
+  const { confirmMessage, onClickDelete, onClickCancel, children } = props;
+  const [showModal, setShowModal] = useState(false);
+
+  // 削除・キャンセルいずれかが押下されたら
+  // モーダルを非表示にする
+  function handleDelete() {
+    onClickDelete();
+    setShowModal(false);
+  }
+
+  function handleCancel() {
+    if(onClickCancel) {
+      onClickCancel();
+    }
+    setShowModal(false);
+  }
+
+  return(
+    <>
+      <DangerButton
+        onClick={ () => { setShowModal(true) }}
+      >
+        { children }
+      </DangerButton>
+      <ConfirmDeleteModal
+        isActive={ showModal }
+        message={ confirmMessage }
+        onClickDelete={ handleDelete }
+        onClickCancel={ handleCancel }
+      />
+    </>
+  )
+}
