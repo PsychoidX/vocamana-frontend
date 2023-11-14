a414e1207e76482d572f66c842d986fca164a1fa
PsycohidX <psychoidx99@gmail.com>
2023-11-14 22:51:03 +0900

    NewWordFormに「フォーム追加ボタン」を追加
    任意の個数の単語を一括で登録できるよう変更

--- components/words/new-word-form.tsx
+++ components/words/new-word-form.tsx
@@ -1,12 +1,14 @@
 "use client"
 import { addWord } from "@/api/words";
 import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form"
-import { SubmitButton } from "@/components/common/button";
+import { useForm, useFieldArray } from "react-hook-form"
+import { Button, SubmitButton } from "@/components/common/button";
 
-type WordFormValues = {
-  word: string,
-  memo: string
+type FormData = {
+  wordForms: {
+    word: string;
+    memo: string;
+  }[];
 }
 
 export default function NewWordForm(
@@ -14,27 +16,50 @@ export default function NewWordForm(
     redirectTo: string,
 }) {
   const { redirectTo } = props;
-  const { register, handleSubmit } = useForm<WordFormValues>();
   const router = useRouter();
-  const onSubmit = async (data: WordFormValues) => {
-    const word = data.word;
-    const memo = data.memo;
-    const success = await addWord(word, memo);
+  const { control, register, handleSubmit } = useForm<FormData>({
+    defaultValues: {
+      wordForms: [{
+        word: "",
+        memo: "",
+      }]
+  }});
+  const { fields, append } = useFieldArray({
+    control,
+    name: 'wordForms'
+  })
 
-    if(success) {
-      router.push(redirectTo)
-      router.refresh()
+  const onSubmit = async (data: FormData) => {
+    for(const wordForm of data.wordForms) {
+      const word = wordForm.word;
+      const memo = wordForm.memo;
+      await addWord(word, memo);
     }
+    // const success = await addWord(word, memo);
+
+    // if(success) {
+    //   router.push(redirectTo)
+    //   router.refresh()
+    // }
   }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="word">Word:</label>
-      <input {...register("word", {required: true})} />
-
-      <label htmlFor="memo">Memo:</label>
-      <input {...register("memo")} />
-
+      {fields.map((item, index) => (
+        <div key={item.id}>
+          <label htmlFor="word">Word:</label>
+          <input {...register(`wordForms.${index}.word` as const)} />
+          <label htmlFor="memo">Memo:</label>
+          <input {...register(`wordForms.${index}.memo` as const)} />
+        </div>
+      ))}
+      <Button
+        onClick={ () => {
+          append({
+            word: "",
+            memo: "",
+          })
+        }}>フォーム追加</Button>
       <SubmitButton>送信</SubmitButton>
     </form>
   );
