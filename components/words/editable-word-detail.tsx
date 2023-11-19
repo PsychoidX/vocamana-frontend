beae1076b46e22c596d6a7acb8be7787e3f8fe0c
PsycohidX <psychoidx99@gmail.com>
2023-11-19 14:03:23 +0900

    Word更新時、画面を再描画するよう変区

--- components/words/editable-word-detail.tsx
+++ components/words/editable-word-detail.tsx
@@ -3,29 +3,33 @@ import { useState } from "react";
 import UpdateWordForm from "./update-word-form";
 import { GrayFlatBox } from "@/components/common/box";
 import { IconButton } from "../common/button";
+import { useRouter } from "next/navigation";
 
 // Wordのwordとmemoを表示
 // wordの右側には編集ボタンを表示し、クリックでwordとmemoを同時編集可能
 export default function EditableWordDetail(props: { word: Word }) {
   const { word } = props;
   const [isEditing, setIsEditing] = useState(false); // 編集中はtrue
-  const [latestWord, setLatestWord] = useState(word); // 更新した場合の最新状態のWordを管理
+  const router = useRouter();
 
   // memoが登録されている場合にのみGlayFlatBoxで表示
   let wordMemoBox: React.ReactNode|undefined;
-  if(latestWord && latestWord.memo.length !== 0) {
-    wordMemoBox = <GrayFlatBox content={ latestWord.memo } />
+  if(word && word.memo.length !== 0) {
+    wordMemoBox = <GrayFlatBox content={ word.memo } />
   }
 
   function onAfterSubmit(updatedWord: Word): void {
-    setLatestWord(updatedWord)
+    // Wordが更新された場合、
+    // 変更をリンク作成に再反映させるため、画面を再読み込み
+    router.push(`/words/${word.id}`);
+    router.refresh();
     setIsEditing(false);
   }
 
   if(isEditing) {
     return(
       <UpdateWordForm
-        word={ latestWord }
+        word={ word }
         onAfterSubmit={onAfterSubmit}
         onClickCancel={ () => { setIsEditing(false) }}
       />
@@ -34,7 +38,7 @@ export default function EditableWordDetail(props: { word: Word }) {
     return (
       <>
         <h2>
-          <span>{ latestWord.word }</span>
+          <span>{ word.word }</span>
           <IconButton
             iconClassNames="fa-solid fa-pen"
             onClick={ () => { setIsEditing(true) }}
