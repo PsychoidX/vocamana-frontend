7ea9a73794a3b5c2c5dfce5746ece4815022bb84
PsycohidX <psychoidx99@gmail.com>
2023-11-26 04:33:07 +0900

    Paginationコンポーネントを作成

---
+++ components/common/pagination.tsx
@@ -0,0 +1,125 @@
+'use client'
+import classNames from "classnames";
+
+export default function Pagination(props: {
+  currentPage: number,
+  maxPage: number,
+}) {
+  const { currentPage, maxPage } = props;
+  
+  if(maxPage < 5) {
+    // ページ数が5未満の場合、全ページ表示
+    // 例：[1][2][3][4]
+    const pages = Array.from({length: maxPage}, (_, i) => i+1)
+    return(
+      <nav className="pagination">
+        <ul className="pagination-list">
+          {pages.map((page) => (
+            <PaginationLink
+              currentPage={ currentPage }
+              page={ page }
+            />
+          ))}
+        </ul>
+      </nav>
+    )
+  } else if(currentPage === 1) {
+    // 最初のページを表示中の場合、
+    // 最初の3ページを表示し、最後のみ省略
+    // 例：[1][2][3]...[10]
+    const pages = [1, 2, 3]
+    return(
+      <nav className="pagination">
+        <ul className="pagination-list">
+          {pages.map((page) => (
+            <PaginationLink
+              currentPage={ currentPage }
+              page={ page }
+            />
+          ))}
+
+          <li><span className="pagination-ellipsis">&hellip;</span></li>
+        
+          <PaginationLink
+            currentPage={ currentPage }
+            page={ maxPage }
+          />
+        </ul>
+      </nav>
+    )
+  } else if (currentPage === maxPage) {
+    // 最後のページを表示中の場合、
+    // 最後の3ページを表示し、最初だけ省略
+    // 例：[1]...[8][9][10]
+    const pages = [maxPage-2, maxPage-1, maxPage]
+    return(
+      <nav className="pagination">
+        <ul className="pagination-list">
+          <PaginationLink
+            currentPage={ currentPage }
+            page={ 1 }
+          />
+
+          <li><span className="pagination-ellipsis">&hellip;</span></li>
+
+          {pages.map((page) => (
+            <PaginationLink
+              currentPage={ currentPage }
+              page={ page }
+            />
+          ))}
+        </ul>
+      </nav>
+    )
+  } else {
+    // 前後3ページを表示し、最初と最後を省略
+    // 例：[1]...[4][5][6]...[10]
+    const pages = [currentPage-1, currentPage, currentPage+1]
+    return(
+      <nav className="pagination">
+        <ul className="pagination-list">
+          <PaginationLink
+            currentPage={ currentPage }
+            page={ 1 }
+          />
+
+          <li><span className="pagination-ellipsis">&hellip;</span></li>
+
+          {pages.map((page) => (
+            <PaginationLink
+              currentPage={ currentPage }
+              page={ page }
+            />
+          ))}
+
+          <li><span className="pagination-ellipsis">&hellip;</span></li>
+
+          <PaginationLink
+            currentPage={ currentPage }
+            page={ maxPage }
+          />
+        </ul>
+      </nav>
+    )
+  }
+}
+
+function PaginationLink(props: {
+  currentPage: number,
+  page: number
+}) {
+  const { currentPage, page } = props;
+  return(
+    <li>
+      <a
+        className={classNames(
+          "pagination-link",
+          { "is-current": page===currentPage }
+        )}
+        href={`?page=${page}`}
+      >
+        { page }
+      </a>
+    </li>
+  )
+}
